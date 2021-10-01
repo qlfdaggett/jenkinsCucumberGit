faae1241d7ec187cc01729d28128bb644832195d
Forrest Daggett <fdaggett@MBP-23.local>
2021-10-01 14:41:32 -0700

    test

--- Build.sh
+++ Build.sh
@@ -1,4 +1,3 @@
 echo "Checking out from github"
-git --help
-git branch: 'main', url: 'https://github.com/qlfdaggett/MavenTest.git'
+git clone https://github.com/qlfdaggett/MavenTest.git
 mvn package
