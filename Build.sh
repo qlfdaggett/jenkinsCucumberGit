170a20e3a1d4d80bb207f24ee4f1ce6d3b015514
Forrest Daggett <fdaggett@MBP-23.local>
2021-10-01 14:16:57 -0700

    test

--- Build.sh
+++ Build.sh
@@ -1,3 +1,6 @@
 echo "Checking out from github"
 git branch 'main', url: 'https://github.com/qlfdaggett/MavenTest.git'
+sh "export MAVEN_HOME=/usr/local/apache-maven"
+sh "export PATH=$PATH:$MAVEN_HOME/bin"
+sh "mvn --version"
 sh "mvn package"
