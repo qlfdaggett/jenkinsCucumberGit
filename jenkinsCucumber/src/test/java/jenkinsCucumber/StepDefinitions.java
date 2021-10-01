341ccd97572ad71d77c27c1a57169cca2b53f7c5
Forrest Daggett <fdaggett@MBP-23.local>
2021-10-01 15:52:32 -0700

    test

--- jenkinsCucumber/src/test/java/jenkinsCucumber/StepDefinitions.java
+++ jenkinsCucumber/src/test/java/jenkinsCucumber/StepDefinitions.java
@@ -7,15 +7,26 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinitions {
+	WebDriver driver;
+	
+	String chromeProp = "webdriver.chrome.driver";
+	String chromeDriverPath = "/Users/fdaggett/Documents/WebDriver/chromedriver";
+
+	String baseUrl = "http://localhost:8081/dev/";
 	
 	@Given("I am testing")
 	public void i_am_testing() {
+		System.setProperty(chromeProp, chromeDriverPath);
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
 	}
 	
 	@Then("I am done testing")
 	public void i_am_done_testing() {
-		Assert.assertTrue(false);
+		Assert.assertTrue(true);
 	}
 }
