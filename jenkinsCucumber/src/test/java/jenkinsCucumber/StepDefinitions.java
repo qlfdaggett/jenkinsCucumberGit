62235d512999eea6fc431ed501528840fe7e1598
Forrest Daggett <fdaggett@MBP-23.local>
2021-10-01 15:46:04 -0700

    test

--- jenkinsCucumber/src/test/java/jenkinsCucumber/StepDefinitions.java
+++ jenkinsCucumber/src/test/java/jenkinsCucumber/StepDefinitions.java
@@ -7,5 +7,12 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
 public class StepDefinitions {
-
+	
+	@Given("I am testing")
+	public void i_am_testing() {
+	}
+	
+	@Then("I am done testing")
+	public void i_am_done_testing() {
+	}
 }
