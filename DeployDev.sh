bd55dab25cd6a552112da0b1668449d42f4ba99a
Forrest Daggett <fdaggett@MBP-23.local>
2021-10-01 15:20:21 -0700

    test

--- DeployDev.sh
+++ DeployDev.sh
@@ -1,2 +1,2 @@
 echo "deploying to dev server"
-/usr/local/bin/wget --http-user=admin --http-password=1234 "http://localhost:8081/manager/text/deploy?war=file:MavenTest/target/mavenTest.war&path=/dev"
+/usr/local/bin/wget --http-user=admin --http-password=1234 "http://localhost:8081/manager/text/deploy?war=file:/Users/fdaggett/.jenkins/workspace/Jenkins_Cucumber_Pipeline/MavenTest/target/mavenTest.war&path=/dev"
